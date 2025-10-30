db36307e7f054ecbc5c1f65b2930e5284d01d356
Ilqi63 <muhammadilqimuzaki@gmail.com>
2025-10-30 11:59:16 +0700

    Replace Mobil class with BankAccount class

--- 02.js
+++ 02.js
@@ -1,21 +1,20 @@
-class Mobil {
-  // Konstruktor untuk inisialisasi objek
-  constructor(merek, model, tahun) {
-    this.merek = merek;   // Properti objek
-    this.model = model;
-    this.tahun = tahun;
-  }
-
-  // Metode untuk menampilkan informasi mobil
-  displayInfo() {
-    console.log(`Mobil: ${this.merek} ${this.model} (${this.tahun})`);
-  }
+class BankAccount {
+#saldo; // Properti private
+constructor(nama, saldoAwal) {
+this.nama = nama;
+this.#saldo = saldoAwal;
+}
+getSaldo() {
+return `Saldo ${this.nama} adalah Rp${this.#saldo}`;
+}
+deposit(jumlah) {
+this.#saldo += jumlah;
+}
 }
 
-// Instansiasi objek dari kelas Mobil
-const mobil1 = new Mobil('Toyota', 'Corolla', 2020); // Membuat objek mobil1
-const mobil2 = new Mobil('Honda', 'Civic', 2021);   // Membuat objek mobil2
-
-// Memanggil metode untuk menampilkan informasi
-mobil1.displayInfo(); // Output: Mobil: Toyota Corolla (2020)
-mobil2.displayInfo(); // Output: Mobil: Honda Civic (2021)
+const akun = new BankAccount("Alice", 500000);
+console.log(akun.getSaldo());
+akun.deposit(200000);
+console.log(akun.getSaldo());
+// console.log(akun.#saldo); // Akan error karena saldo bersifat
+private
